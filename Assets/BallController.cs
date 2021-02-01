a0353118c84d246d4d47325feecaf91a4d2e8256
Kei-Saito2 <77094528+Kei-Saito2@users.noreply.github.com>
2021-02-01 20:01:50 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+
+    private GameObject pointText;
+    int point = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.pointText = GameObject.Find("PointText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + " pt";
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string ObjectTag = collision.gameObject.tag;
+        if (ObjectTag == "SmallStarTag")
+        {
+            this.point += 10;
+        }
+        else if (ObjectTag == "LargeStarTag")
+        {
+            this.point += 20;
+        }
+        else if (ObjectTag == "SmallCloudTag")
+        {
+            this.point += 15;
+        }
+        else if(ObjectTag == "LargeCloudTag")
+        {
+            this.point += 50;
+        }
+    }
+}
---
+++ Assets/PointController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour
+{
+    private GameObject pointText;
+    int point = 0;
+
+    public void GetSmallStar()
+    {
+        this.point += 10;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.pointText = GameObject.Find("PointText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + " pt";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
